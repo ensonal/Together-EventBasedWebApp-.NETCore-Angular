bb725200376b6969d39db22b3be78d7937feb144
Enes Önal <enes.onal777@gmail.com>
2024-03-15 22:23:10 +0300

    feat: add material, create navbar and implement it

--- Frontend/togetherapp/src/app/app.routes.ts
+++ Frontend/togetherapp/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Route } from '@angular/router';
 import { LoginComponent } from './auth-management/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { RegisterComponent } from './auth-management/register/register.component';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
+    // localhost:4200/register
     {
         path: "",
-        redirectTo: "login",
+        redirectTo: "home",
         pathMatch: "full"
     },
     {
@@ -14,12 +16,16 @@ export const appRoutes: Route[] = [
         component: LoginComponent,
     },
     {
+        path: "register",
+        component: RegisterComponent,
+    },
+    {
         path: "",
         component: LayoutComponent,
         children: [
             {
-                path: "registers",
-                component: RegisterComponent
+                path: "home",
+                component: HomeComponent
             }
         ]
     }
