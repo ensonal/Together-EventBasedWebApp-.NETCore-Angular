d17d1cba23d065c728838e78e5c26adf1325bf6a
Enes Önal <enes.onal777@gmail.com>
2024-03-17 05:13:41 +0300

    feat: create my account edit page

--- Frontend/togetherapp/src/app/profile/components/main-content-tabs/main-content-tabs.component.ts
+++ Frontend/togetherapp/src/app/profile/components/main-content-tabs/main-content-tabs.component.ts
@@ -5,11 +5,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MySportsComponent } from '../../../my-sports/my-sports-home/my-sports.component';
 import { MyEventsHomeComponent } from 'src/app/my-events/my-events-home/my-events-home.component';
+import { MyAccountComponent } from 'src/app/my-account/my-account-home/my-account.component';
 
 @Component({
   selector: 'togetherapp-main-content-tabs',
   standalone: true,
-  imports: [CommonModule, MatTabsModule, MatCardModule, MatDividerModule, MySportsComponent, MyEventsHomeComponent],
+  imports: [CommonModule, MatTabsModule, MatCardModule, MatDividerModule, MySportsComponent, MyEventsHomeComponent, MyAccountComponent],
   templateUrl: './main-content-tabs.component.html',
   styleUrl: './main-content-tabs.component.scss',
 })
