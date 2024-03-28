0cbb3d85e7d4a314add1af97ef8bb0f17e077b69
Enes Önal <enes.onal777@gmail.com>
2024-03-29 01:33:03 +0300

    feat: create register page

--- Frontend/togetherapp/src/app/auth-management/register/register-home/register.component.ts
+++ Frontend/togetherapp/src/app/auth-management/register/register-home/register.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'togetherapp-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
