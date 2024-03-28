f7a4168c7aac71df2fade95a8ae35984f3e47747
Enes Önal <enes.onal777@gmail.com>
2024-03-29 02:48:19 +0300

    refact: add related components into home page

--- Frontend/togetherapp/src/app/home/home.component.ts
+++ Frontend/togetherapp/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { EventListComponent } from './components/event-list/event-list.component';
 
 @Component({
   selector: 'togetherapp-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, SearchBarComponent, CategoryListComponent, EventListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
