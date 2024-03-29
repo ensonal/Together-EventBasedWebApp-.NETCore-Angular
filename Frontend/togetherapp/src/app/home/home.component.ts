7a861fe62c394e70d59c7e4ab8e53ce0695b8ceb
Enes Önal <enes.onal777@gmail.com>
2024-03-29 03:26:30 +0300

    feat: create banners component

--- Frontend/togetherapp/src/app/home/home.component.ts
+++ Frontend/togetherapp/src/app/home/home.component.ts
@@ -5,11 +5,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { EventListComponent } from './components/event-list/event-list.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { AdsBannerComponent } from './components/ads-banner/ads-banner.component';
 
 @Component({
   selector: 'togetherapp-home',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, SearchBarComponent, CategoryListComponent, EventListComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule, SearchBarComponent, CategoryListComponent, EventListComponent, MatDividerModule, AdsBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
