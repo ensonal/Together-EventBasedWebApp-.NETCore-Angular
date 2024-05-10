bfaf6be8401dec5c4db5f7031bbbdc3b20f28daa
Enes Önal <enes.onal777@gmail.com>
2024-05-10 22:13:37 +0300

    fix: user ui bug

--- FrontendReact/together-app/src/routes/MyProfile/UserInfo/components/UserCarHeader.tsx
+++ FrontendReact/together-app/src/routes/MyProfile/UserInfo/components/UserCarHeader.tsx
@@ -6,7 +6,7 @@ export function UserCardHeader({ userInfo }: { userInfo: IUserInfo }) {
   return (
     <>
       <div className="d-flex flex-column justify-content-center">
-        <div className="d-flex flex-column justify-content-center mt-3">
+        <div className="d-flex flex-column align-items-center justify-content-center mt-3">
           <img
             className="rounded-circle"
             src={ProfilePic}
@@ -15,9 +15,9 @@ export function UserCardHeader({ userInfo }: { userInfo: IUserInfo }) {
             alt="Profile"
             style={{ objectFit: "cover" }}
           />
-          <h4 className="text-center mt-3 m-0">
+          <p className="fs-4 fw-normal text-center mt-3 m-0">
             {userInfo.name} {userInfo.surname}
-          </h4>
+          </p>
         </div>
       </div>
       <Divider className="mt-3 mb-3"/>
