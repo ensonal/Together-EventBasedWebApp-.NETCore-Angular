78794e437ccab130b306f4eefd58f3ebed487ce1
Enes Önal <enes.onal777@gmail.com>
2024-05-18 02:12:00 +0300

    fix: event home page style

--- FrontendReact/together-app/src/routes/Events/EventsPage.tsx
+++ FrontendReact/together-app/src/routes/Events/EventsPage.tsx
@@ -1,10 +1,12 @@
 import { EventGrid } from "./components/EventGrid/EventGrid";
 import { EventSearchCard } from "./components/EventSearchCard/EventSearchCard";
+import Pagination from "@mui/material/Pagination";
 
 export function EventsPage() {
   return (
-    <div className="d-flex flex-column gap-3 align-self-start">
+    <div className="d-flex flex-column gap-3 align-self-start align-items-center">
       <EventSearchCard />
+      <Pagination count={10} showFirstButton showLastButton />
       <EventGrid />
     </div>
   );
--- FrontendReact/together-app/src/routes/Events/components/EventSearchCard/EventSearchCard.tsx
+++ FrontendReact/together-app/src/routes/Events/components/EventSearchCard/EventSearchCard.tsx
@@ -5,7 +5,7 @@ import { AdditionalFilters } from "./AdditionalFilters";
 export function EventSearchCard() {
   return (
     <Card
-      className="p-3 rounded-3 d-flex flex-column gap-2"
+      className="p-3 rounded-3 d-flex flex-column gap-2 w-100"
       sx={{ boxShadow: 0 }}
     >
       <QuerySearch />
