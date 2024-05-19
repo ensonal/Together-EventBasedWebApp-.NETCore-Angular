a70115ac3e7ae48487979fe4030e8a4e015bf31b
Enes Önal <enes.onal777@gmail.com>
2024-05-19 22:38:55 +0300

    fix: event filter style

--- FrontendReact/together-app/src/routes/EventHome/components/EventSearchCard/EventSearchCard.tsx
+++ FrontendReact/together-app/src/routes/EventHome/components/EventSearchCard/EventSearchCard.tsx
@@ -2,15 +2,18 @@ import { Card } from "@mui/material";
 import { QuerySearch } from "./QuerySearch";
 import { AdditionalFilters } from "./AdditionalFilters";
 import { EventFilters } from "../../../../api/models/EventModels/EventFilters";
+import { useState } from "react";
 
 export function EventSearchCard({ filters, setFilters }: { filters: EventFilters, setFilters: any }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <Card
       className="p-3 rounded-3 d-flex flex-column gap-2 w-100"
       sx={{ boxShadow: 0 }}
     >
-      <QuerySearch filters={filters} setFilters={setFilters}/>
-      <AdditionalFilters filters={filters} setFilters={setFilters}/>
+      <QuerySearch setFilters={setFilters} setOpen={setOpen} open={open} />
+      {open && <AdditionalFilters filters={filters} setFilters={setFilters}/>}
     </Card>
   );
 }
--- FrontendReact/together-app/src/routes/EventHome/components/EventSearchCard/QuerySearch.tsx
+++ FrontendReact/together-app/src/routes/EventHome/components/EventSearchCard/QuerySearch.tsx
@@ -1,11 +1,11 @@
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import FilterListRoundedIcon from "@mui/icons-material/FilterListRounded";
 import { IconButton } from "@mui/material";
 import { EventFilters } from "../../../../api/models/EventModels/EventFilters";
 
-export function QuerySearch({ filters, setFilters }: { filters: EventFilters, setFilters: any }) {
+export function QuerySearch({ setFilters, setOpen, open }: { setFilters: any, setOpen: (open: boolean) => void, open: boolean }) {
   const handleFilterChange = (name: string, value: any) => {
     setFilters((prevFilters: EventFilters) => ({
       ...prevFilters,
@@ -30,8 +30,12 @@ export function QuerySearch({ filters, setFilters }: { filters: EventFilters, se
         size="small"
         onChange={(e) => handleFilterChange("searchQuery", e.target.value)}
       />
-      <IconButton className="rounded-2" sx={{ border: "1px solid #3D52F3" }}>
-        <KeyboardDoubleArrowRightIcon color="primary" fontSize="small" />
+      <IconButton
+        className="rounded-2"
+        sx={{ border: "1px solid #909090" }}
+        onClick={() => setOpen(!open)}
+      >
+        <FilterListRoundedIcon style={{ color: "#707070" }} fontSize="small" />
       </IconButton>
     </div>
   );
