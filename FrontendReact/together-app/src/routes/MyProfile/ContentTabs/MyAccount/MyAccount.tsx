8b54e29c858ab23ede1d5e6ef478e165834b884b
Enes Önal <enes.onal777@gmail.com>
2024-05-20 04:28:39 +0300

    feat: add loading logic and snackbar

--- FrontendReact/together-app/src/routes/MyProfile/ContentTabs/MyAccount/MyAccount.tsx
+++ FrontendReact/together-app/src/routes/MyProfile/ContentTabs/MyAccount/MyAccount.tsx
@@ -1,5 +1,5 @@
 import TextField from "@mui/material/TextField";
-import { Divider } from "@mui/material";
+import { Alert, Box, Divider, Skeleton, Snackbar } from "@mui/material";
 import InputAdornment from "@mui/material/InputAdornment";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
@@ -13,28 +13,61 @@ import { getUser } from "./hooks/getUser";
 import { updateUser } from "./hooks/updateUser";
 import IUserInfo from "../../../../api/models/UserInfo";
 import { UploadProfileImage } from "./components/UploadProfileImage";
+import { MyAccountSkeleton } from "./components/MyAccountSkeleton";
+import React from "react";
+import { SnackbarOrigin } from "@mui/material/Snackbar";
+
+interface State extends SnackbarOrigin {
+  open: boolean;
+  message: string;
+  severity: "success" | "error";
+}
 
 export function MyAccount() {
   const [userInfo, setUserInfo] = useState<IUserInfo | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+  const [state, setState] = React.useState<State>({
+    open: false,
+    vertical: "bottom",
+    horizontal: "center",
+    message: "",
+    severity: "success",
+  });
+
+  const { vertical, horizontal, open, message, severity} = state;
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        setLoading(true);
         const userData = await getUser()();
         setUserInfo(userData || undefined);
       } catch (error) {
-        console.error("Failed to fetch user information:", error);
-        setUserInfo(undefined);
+        setState({ ...state, open: true, message: "Failed to fetch user information", severity: "error" });
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchUser();
   }, []);
 
-  const handleSave = () => {
+  const handleSave = async () => {
     if (userInfo) {
-      updateUser(userInfo);
+      try {
+        setLoading(true);
+        await updateUser(userInfo);
+        setState({ ...state, open: true, message: "User information updated successfully", severity: "success" });
+      } catch (error) {
+        setState({ ...state, open: true, message: "Failed to update user information", severity: "error" });
+      } finally {
+        setLoading(false);
+      }
     }
+  };
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
   };
 
   const handleInputChange = (key: keyof IUserInfo, value: string) => {
@@ -47,14 +80,16 @@ export function MyAccount() {
   };
 
   if (!userInfo) {
-    return <div>Loading...</div>;
+    return <div>No user information available</div>;
   }
 
-  return (
+  return loading ? (
+    <MyAccountSkeleton />
+  ) : (
     <Card sx={{ padding: 3, paddingBottom: 1, width: "100%", boxShadow: 0 }}>
       <div className="rounded-3 w-100 pt-2 pb-2">
         <div className="d-flex flex-column justify-content-between">
-          <UploadProfileImage profileImageUrl={userInfo.profileImageUrl} />
+          <UploadProfileImage profileImageUrl={userInfo?.profileImageUrl} />
           <Divider />
           <div className="forms pt-4 d-flex flex-column gap-1">
             <div className="d-flex flex-row gap-3">
@@ -72,7 +107,7 @@ export function MyAccount() {
                       </InputAdornment>
                     ),
                   }}
-                  value={userInfo.name}
+                  value={userInfo?.name}
                   onChange={(e) => handleInputChange("name", e.target.value)}
                   fullWidth
                 />
@@ -131,7 +166,7 @@ export function MyAccount() {
                       </InputAdornment>
                     ),
                   }}
-                  value={userInfo.phoneNumber}
+                  value={userInfo?.phoneNumber}
                   onChange={(e) =>
                     handleInputChange("phoneNumber", e.target.value)
                   }
@@ -154,7 +189,7 @@ export function MyAccount() {
                       </InputAdornment>
                     ),
                   }}
-                  value={userInfo.city}
+                  value={userInfo?.city}
                   onChange={(e) => handleInputChange("city", e.target.value)}
                   fullWidth
                 />
@@ -173,7 +208,7 @@ export function MyAccount() {
                       </InputAdornment>
                     ),
                   }}
-                  value={userInfo.country}
+                  value={userInfo?.country}
                   onChange={(e) => handleInputChange("country", e.target.value)}
                   fullWidth
                 />
@@ -184,13 +219,25 @@ export function MyAccount() {
             <Button
               variant="contained"
               className="mt-2"
-              onClick={() => handleSave()}
+              onClick={handleSave}
+              disabled={loading}
             >
               Save Changes
             </Button>
           </div>
         </div>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        autoHideDuration={5000}
+        key={"bottom center"}
+        onClose={handleClose}
+      >
+        <Alert severity={severity} variant="filled" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </Card>
   );
 }
