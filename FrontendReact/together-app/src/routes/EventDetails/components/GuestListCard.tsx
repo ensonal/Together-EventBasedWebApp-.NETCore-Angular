0c0f9adc0c1872014b88216e45022ed3f26f1b07
Enes Önal <enes.onal777@gmail.com>
2024-06-03 01:54:03 +0300

    fix: image url to guestlist

--- FrontendReact/together-app/src/routes/EventDetails/components/GuestListCard.tsx
+++ FrontendReact/together-app/src/routes/EventDetails/components/GuestListCard.tsx
@@ -23,7 +23,7 @@ export function GuestListCard({ guests }: { guests: any[] }) {
               <div key={index} className="d-flex flex-column gap-2 mt-2" style={{ cursor: "pointer"}} onClick={() => handleClick(guest.userID)}>
                 <Box className="d-flex flex-row gap-3 p-0">
                   <img
-                    src={guest.profileImageUrl}
+                    src={guest.profileImageUrl ? guest.profileImageUrl : "https://togetherwebapp.blob.core.windows.net/userprofileimages/people.png"}
                     alt="guest"
                     className="rounded-circle"
                     style={{ width: 50, height: 50, objectFit: "cover" }}
