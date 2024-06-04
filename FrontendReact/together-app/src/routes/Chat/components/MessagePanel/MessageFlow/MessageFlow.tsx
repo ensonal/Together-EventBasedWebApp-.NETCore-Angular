8d43847b2fc34dfca44b115c2859acbb33b4a8c4
Enes Önal <enes.onal777@gmail.com>
2024-06-04 04:12:55 +0300

    fix: style

--- FrontendReact/together-app/src/routes/Chat/components/MessagePanel/MessageFlow/MessageFlow.tsx
+++ FrontendReact/together-app/src/routes/Chat/components/MessagePanel/MessageFlow/MessageFlow.tsx
@@ -6,7 +6,7 @@ export function MessageFlow() {
   return (
     <div className="message-flow">
       <div className=" d-flex flex-column ps-3 p-2">
-        {Array.from({ length: 2 }).map((_, index) => (
+        {Array.from({ length: 1 }).map((_, index) => (
           <div key={index}>
             <IncomingMessageCard />
           </div>
